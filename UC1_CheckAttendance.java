369e960b5768104c34f40a533abf54f6a0449e29
amartarocks3 <amartarocks3@gmail.com>
2022-04-27 08:23:02 +0530

    Added : To check present or absent

---
+++ UC1_CheckAttendance.java
@@ -0,0 +1,13 @@
+package EmployeeWage;
+
+public class UC1_CheckAttendance {
+public static void main(String[] args) {	
+int IS_FULL_TIME = 1;
+int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+if(empCheck == IS_FULL_TIME) {
+	System.out.println("Employee is present");
+}
+else {
+	System.out.println("Employee is abscent");
+}
+}}
