6461c8dd2523b9eacd8aa06ab51b29ba7416e8e7
amartarocks3 <amartarocks3@gmail.com>
2022-04-27 08:30:03 +0530

    Added : To calculate daily wage with parttime using switch case

--- UC3_addPartTime.java
+++ UC3_addPartTime.java
@@ -1,25 +1,25 @@
 package EmployeeWage;
 
 public class UC3_addPartTime {
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 	public static void main(String[] args) {	
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME =2;
-		int EMP_RATE_PER_HOUR = 20;
+		
+		
 		int empHrs = 0;
 		int empWage = 0;
 		
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
-			empHrs = 8;
-		}
-		else if(empCheck ==  IS_PART_TIME) {
-			System.out.println("Employee is present");
+		switch(empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-		}
-		else {
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
 			empHrs = 0;
-			System.out.println("Employee is abscent");
 		}
 		empWage =empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage " + empWage);
